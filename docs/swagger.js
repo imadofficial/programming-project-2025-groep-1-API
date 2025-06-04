0f678a39b3ec9585791828c957738be85c63c0ec
Hublack <71221641+HublackBE@users.noreply.github.com>
2025-06-04 13:17:05 +0200

    feat: add OpenAPI documentation and integrate YAML loader for Swagger instead of JSDoc

--- docs/swagger.js
+++ docs/swagger.js
@@ -1,26 +1,10 @@
 const swaggerUi = require('swagger-ui-express');
-const swaggerJSDoc = require('swagger-jsdoc');
+const YAML = require('yamljs');
 
-const swaggerDefinition = {
-  openapi: '3.1.0',
-  info: {
-    title: 'My API',
-    version: '1.0.0',
-    description: 'Demo Express API documented with OpenAPI 3.1 & Swagger UI',
-  },
-  servers: [{ url: 'http://localhost:3001', description: 'Dev server' }],
-};
-
-const options = {
-  definition: swaggerDefinition,
-  // Globs to every file that contains JSDoc @openapi blocks:
-  apis: ['./routes/**/*.js', './models/**/*.js'],
-};
-
-const swaggerSpec = swaggerJSDoc(options);
+const openAPIDocs = YAML.load('./docs/openapi.yaml');
 
 const swaggerDocs = (app) => {
-  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(openAPIDocs));
   console.log(`📚  Swagger docs available at http://localhost:3001/docs`);
 };
 
