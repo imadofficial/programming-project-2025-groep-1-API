8409ec87af30a8929641e57d333b37b22866480a
Hublack <71221641+HublackBE@users.noreply.github.com>
2025-06-11 19:57:38 +0200

    refactor: simplify uploadthing module by removing unused key generation functions

--- src/modules/uploadthing.js
+++ src/modules/uploadthing.js
@@ -1,59 +1,9 @@
 const { UTApi } = require('uploadthing/server');
+require('dotenv').config(); // Ensure you have dotenv to load environment variables
 
-const utapi = new UTApi();
 
-// This module provides a function to generate a unique file key for UploadThing uploads
-const SQIds = require("sqids");
-const { defaultOptions } = require("sqids");
+const utapi = new UTApi({
+  token: process.env.UPLOADTHING_API_KEY, // Your UploadThing API key
+});
 
-function djb2(s) {
-  let h = 5381;
-  let i = s.length;
-  while (i) {
-    h = (h * 33) ^ s.charCodeAt(--i);
-  }
-  return (h & 0xbfffffff) | ((h >>> 1) & 0x40000000);
-}
-
-// A simple function to shuffle the alphabet for the Sqids
-function shuffle(str, seed) {
-  const chars = str.split("");
-  const seedNum = djb2(seed);
-
-  let temp;
-  let j;
-  for (let i = 0; i < chars.length; i++) {
-    j = ((seedNum % (i + 1)) + i) % chars.length;
-    temp = chars[i];
-    chars[i] = chars[j];
-    chars[j] = temp;
-  }
-
-  return chars.join("");
-}
-
-// Helper for base64 encoding (URL safe)
-function encodeBase64(str) {
-  return Buffer.from(str, "utf8")
-    .toString("base64")
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_")
-    .replace(/=+$/, "");
-}
-
-function generateKey(appId, fileSeed) {
-  // Hash and Encode the parts and apiKey as sqids
-  const alphabet = shuffle(defaultOptions.alphabet, appId);
-
-  const encodedAppId = new SQIds({ alphabet, minLength: 12 }).encode([
-    Math.abs(djb2(appId)),
-  ]);
-
-  // We use a base64 encoding here to ensure the file seed is url safe, but
-  // you can do this however you want
-  const encodedFileSeed = encodeBase64(fileSeed);
-
-  return `${encodedAppId}${encodedFileSeed}`;
-}
-
-module.exports = { utapi, generateKey };
+module.exports = { utapi };
