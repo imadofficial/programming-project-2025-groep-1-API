a862852032f9afb4bfb778e7dbc288d7939f9ff4
Hublack <71221641+HublackBE@users.noreply.github.com>
2025-06-12 10:25:31 +0200

    refactor: remove unused fs and path imports from studenten route file

--- src/routes/studenten.js
+++ src/routes/studenten.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 const passport = require('passport');
 const { getAllStudenten, getStudentById } = require('../sql/studenten.js');
 
@@ -8,7 +6,6 @@ require('../auth/passportJWT.js');
 
 const router = express.Router()
 
-const studentenData = JSON.parse(fs.readFileSync(path.join('data/studentenlijst.json'), 'utf8'));
 
 router.get('/', passport.authenticate('jwt', { session: false }), async (req, res) => {
     for (const [param, value] of Object.entries(req.query)) {
