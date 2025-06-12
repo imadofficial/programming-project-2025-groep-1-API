ec339d944c1f5ea0649e215bdd5079d964823332
Brend Van Den Eynde <brend.vandeneynde@outlook.com>
2025-06-12 13:57:59 +0200

    Update bedrijven.js

--- src/routes/bedrijven.js
+++ src/routes/bedrijven.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const passport = require('passport');
-const { getAllBedrijven, getBedrijfById, getGoedgekeurdeBedrijven } = require('../sql/bedrijven.js');
+const { getAllBedrijven, getBedrijfById, getGoedgekeurdeBedrijven, getNietGoedgekeurdeBedrijven, keurBedrijfGoed } = require('../sql/bedrijven.js');
 
 require('../auth/passportJWT.js');
 
@@ -13,11 +13,32 @@ router.get('/', passport.authenticate('jwt', { session: false }), async (req, re
     }
     const bedrijven = await getAllBedrijven();
     res.json(bedrijven);
-})
+});
 
 router.get('/:bedrijfID', passport.authenticate('jwt', { session: false }), async (req, res) => {
     const bedrijf = await getBedrijfById(req.params['bedrijfID']);
     res.json(bedrijf);
-})
+});
+
+router.get('/goedgekeurd', passport.authenticate('jwt', { session: false }), async (req, res) => {
+   const bedrijven = await getGoedgekeurdeBedrijven();
+   res.json(bedrijven);
+});
+
+router.get('/nietgoedgekeurd', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    const bedrijven = await getNietGoedgekeurdeBedrijven();
+    res.json(bedrijven);
+});
+
+router.post('/keur/:bedrijfID', [passport.authenticate('jwt', { session: false }), authAdmin], async (req, res) => {
+    const bedrijfID = req.params['bedrijfID'];
+    try {
+        await keurBedrijfGoed(bedrijfID);
+        res.status(200).json({ message: 'Bedrijf goedgekeurd' });
+    } catch (error) {
+        console.error('Error approving bedrijf:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
 
 module.exports = router;
