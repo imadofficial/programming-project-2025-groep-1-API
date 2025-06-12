d97b2914c2dc17613483d9eb8c6c8228308fa3c5
Hublack <71221641+HublackBE@users.noreply.github.com>
2025-06-12 11:43:40 +0200

    feat: update passport local strategy to use getUserType for user type retrieval

--- src/auth/passportLocal.js
+++ src/auth/passportLocal.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 const users = require('../../data/users.json'); // Assuming you have a users.json file with user data
-const { login, isAdmin } = require('../sql/login.js'); // Adjust the path as necessary
+const { login, getUserType } = require('../sql/login.js'); // Adjust the path as necessary
 
 console.log("Loading passport local strategy...");
 
@@ -18,7 +18,7 @@ passport.use(new LocalStrategy(
         if (userId === null) {
             return done(null, false, { message: 'Incorrect email or password.' });
         }
-        const isAdminUser = await isAdmin(userId);
-        return done(null, { id: userId, isAdmin: isAdminUser });
+        const type = await getUserType(userId);
+        return done(null, { id: userId, type: type });
     }
 ));
