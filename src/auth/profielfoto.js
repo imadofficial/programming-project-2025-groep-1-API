4686f3983dd4a712efca29d2aca95814b16aa5d1
Hublack <71221641+HublackBE@users.noreply.github.com>
2025-06-13 18:45:53 +0200

    dev

--- src/auth/profielfoto.js
+++ src/auth/profielfoto.js
@@ -1,30 +1,39 @@
 const express = require('express');
 const passport = require('passport');
 const multer = require('multer');
-const utapi = require('../modules/uploadthing.js');
+const { UTApi } = require('uploadthing/server');
 const { UTFile } = require('uploadthing/server');
 
 require('../auth/passportJWT.js');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
-
+const utapi = new UTApi();
 
 // TODO: implement Uploadthing using https://docs.uploadthing.com/api-reference/ut-api
 
-router.post('/', upload.single('image'), passport.authenticate('jwt', { session: false }), async (req, res) => {
+
+async function uploadServerSideFile(file) {
+    console.log('Uploading file:', file);
+    try {
+        const response = await utapi.uploadFiles(file);
+        return response.data
+    } catch (error) {
+        console.error('Error uploading file:', error);
+        throw new Error('File upload failed');
+    }
+}
+
+
+
+router.post('/', upload.single('file'), async (req, res) => {
     console.log('File upload request received:', req.file);
     if (!req.file) {
         return res.status(400).json({ message: 'No file uploaded' });
     }
 
     try {
-        // Assuming utapi.upload is a method to handle file uploads
-        const utFile = new UTFile([req.file.buffer], req.file.originalname, {
-            type: req.file.mimetype,
-            lastModified: Date.now(),
-        });
-        const uploadResult = await utapi.uploadFiles(utFile);
+        const uploadResult = await uploadServerSideFile(req.file);
         res.status(201).json({ message: 'File uploaded successfully', data: uploadResult });
     } catch (error) {
         console.error('Error uploading file:', error);
--- src/modules/uploadthing.js
+++ src/modules/uploadthing.js
@@ -1,9 +1,22 @@
-const { UTApi } = require('uploadthing/server');
+const { createUploadthing } = require('uploadthing/next');
 
 require('dotenv').config();
 
-const utapi = new UTApi({
-    token: process.env.UPLOADTHING_API_TOKEN,
-});
+const f = createUploadthing();
 
-module.exports = utapi;
+const uploadthing = {
+    profilePicture: f({ image: { maxFileSize: '4MB', maxFileCount: 1, minFileCount: 1 } })
+        .middleware(async (req, res, next) => {
+            console.log('Middleware for profile picture upload triggered');
+            //if (!req.user) {
+            //    return res.status(401).json({ message: 'Unauthorized' });
+            //}
+            next();
+        })
+        .onUploadComplete(async (data) => {
+            console.log('File uploaded:', data);
+            // Here you can handle the file, e.g., save it to a database or perform other actions
+        }),
+}
+
+module.exports = uploadthing
